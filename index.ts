d5d4baef3594cd4706151e328fc33d735dd508da
vikram(wsl) <vikramjit360@gmail.com>
2025-10-31 06:06:37 +0530

    feat: change package.json to use tsx for dev and start scripts, cleanup other packages

--- index.ts
+++ index.ts
@@ -1,10 +1,18 @@
-import app from "./app";
-import config from "./utils/env";
-import { connectDB } from "./config/mongodb";
+import app from "./app.js";
+import config from "./utils/env.js";
+import { connectDB, closeDB } from "./config/mongodb.js";
 
 // If the DB connection is successful only then start the express backend
 connectDB().then(() => {
   app.listen(config.PORT, () => {
     console.log(`Server is running on port ${config.PORT}`);
   });
+});
+
+
+// Graceful shutdown
+process.on("SIGTERM", async () => {
+  console.log("SIGTERM received, shutting down gracefully");
+  await closeDB();
+  process.exit(0);
 });
