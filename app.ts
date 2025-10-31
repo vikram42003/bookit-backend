84cfcc47d6a01f6599511682a3a24032f3d980b0
vikram(wsl) <vikramjit360@gmail.com>
2025-11-01 02:40:59 +0530

    feat: add /health endpoint

--- app.ts
+++ app.ts
@@ -10,9 +10,16 @@ import promoCodeRouter from "./routes/promoCodeRoute.js";
 const app = express();
 
 // Middlewares
+// We allow everything in cors for now since this is only a demo app for assignment and there is a time constraint
 app.use(cors());
 app.use(express.json());
 app.use(logger);
+
+// Health checkup endpoint
+// I added this to deal with the slow cold boots on Render free tier, we can call this endpoint as soon as a user visits the app to warm up the server
+app.get("/api/health", (req, res) => {
+  res.json({ message: "All systems are good"})
+})
 
 // Attach routers
 app.use("/api/experiences", experiencesRouter);
